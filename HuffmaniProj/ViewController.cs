a5e9b994b015088d430f6a37f76601d2c1d5a192
BushraNaqviTaimur <syedabushranaqvi@icloud.com>
2021-08-13 03:22:46 +0500

    Final Code

---
+++ HuffmaniProj/ViewController.cs
@@ -0,0 +1,99 @@
+﻿using Foundation;
+using System;
+using UIKit;
+using System.Collections.Generic;
+using System.ComponentModel;
+//using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace HuffmaniProj
+{
+    public partial class ViewController : UIViewController
+    {
+        private int leafNodes;
+
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        partial void BtnGenerate_TouchUpInside(UIButton sender)
+        {
+            string s = textBox1.Text;
+            int n = s.Length;
+            List<CharFreq> list = new List<CharFreq>();
+
+            textBox2.Text = string.Empty;
+
+            for (int i = 0; i < n; i++)
+            {
+                bool found = false;
+                char c = s[i];
+                CharFreq cf = new CharFreq();
+
+                for (int j = 0; !found && j < list.Count; j++)
+                {
+                    if (c == list[j].ch)
+                    {
+                        found = true;
+                        cf.ch = c;
+                        cf.freq = 1 + list[j].freq;
+                        list.RemoveAt(j);
+                        list.Add(cf);
+                    }
+                }
+
+                if (!found)
+                {
+                    cf.ch = c;
+                    cf.freq = 1;
+                    list.Add(cf);
+                }
+            }
+
+            HuffmanTree ht = new HuffmanTree();
+            BinaryTreeNode<CharFreq> root = ht.Build(list, list.Count);
+
+            InorderTraversal(root);
+            textBox2.Text += "\r\n# characters = " + n.ToString() + "\r\n";
+            textBox2.Text += "# leaf nodes = " + leafNodes.ToString() + "\r\n";
+            textBox2.Text += "% compressed = " +
+                (100.0 - 100.0 * ((double)leafNodes) / n).ToString("F2") + "\r\n";
+        }
+
+        private void InorderTraversal(BinaryTreeNode<CharFreq> node)
+        {
+            if (node != null)
+            {
+                InorderTraversal(node.Left);
+
+                CharFreq cf = node.Value;
+                int ord = (int)cf.ch;
+
+                if (node.Left == null && node.Right == null)
+                {
+                    leafNodes++;
+                    textBox2.Text += "'" + new string(cf.ch, 1) + "' ";
+                    textBox2.Text += node.Value.freq.ToString() + "\r\n";
+                }
+
+                InorderTraversal(node.Right);
+            }
+        }
+
+       
+    }
+}
---
+++ HuffmaniProj/ViewController.designer.cs
@@ -0,0 +1,51 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace HuffmaniProj
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton btnGenerate { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField textBox1 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextView textBox2 { get; set; }
+
+        [Action ("BtnGenerate_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void BtnGenerate_TouchUpInside (UIKit.UIButton sender);
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (btnGenerate != null) {
+                btnGenerate.Dispose ();
+                btnGenerate = null;
+            }
+
+            if (textBox1 != null) {
+                textBox1.Dispose ();
+                textBox1 = null;
+            }
+
+            if (textBox2 != null) {
+                textBox2.Dispose ();
+                textBox2 = null;
+            }
+        }
+    }
+}
